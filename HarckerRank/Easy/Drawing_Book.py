59dfd5db2de99438d6bda31141390ef218b4faa3
AluguriSrivalli <78839083+AluguriSrivalli@users.noreply.github.com>
2026-09-17 22:25:56 +0530

    Add Drawing_Book.py with pageCount function
    https://www.hackerrank.com/challenges/drawing-book/problem?isFullScreen=true

---
+++ HarckerRank/Easy/Drawing_Book.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+def pageCount(n, p):
+    # Write your code here
+    from_front = p // 2
+    
+    # Turns from the back
+    from_back = (n // 2) - (p // 2)
+    
+    # Return the minimum of both choices
+    return min(from_front, from_back)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
